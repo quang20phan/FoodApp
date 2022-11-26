c2e9cfd4a73b590c48da0758dbce3280f72c62bc
dongtenquang <tacatv2002@gmail.com>
2022-11-26 20:36:00 +0700

    Done

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.block1}>
+          <Text>
+          Square 1
+          </Text>
+          <StatusBar style="auto" />
+      </View> 
+      <View style={styles.block2}>
+          <Text>
+          Square 2
+          </Text>
+          <StatusBar style="auto" />
+      </View>
+      <View style={styles.block3}>
+          <Text>
+          Square 3
+          </Text>
+          <StatusBar style="auto" />
+      </View>  
+      
     </View>
   );
 }
@@ -15,6 +32,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    flexDirection:'row'
   },
+  block1 : {
+    paddingVertical: 30,
+    paddingHorizontal: 30,
+    backgroundColor: 'green'
+  },
+
+  block2 : {
+    paddingVertical: 30,
+    paddingHorizontal: 30,
+    backgroundColor: 'violet'
+  },
+
+  block3 : {
+    paddingVertical: 30,
+    paddingHorizontal: 30,
+    backgroundColor: 'red'
+  }
 });
